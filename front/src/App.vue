dce9c768bc5ddff2035f22e0d5985b57982fbeba
SeZblaznim <71178555+SeZblaznim@users.noreply.github.com>
2023-01-19 22:17:50 +0100

    front: initial commit

---
+++ front/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, computed } from 'vue'
+import Home from './HomePage.vue'
+import LogIn from './LogInPage.vue'
+
+
+const routes = {
+  '/': Home,
+  '/login': LogIn
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/']
+})
+</script>
+
+<template>
+  <a href="#/">Home</a>
+  <a href="#/login">Log in</a>
+  <component :is="currentView" />
+</template> 
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/LogInPage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+function toHomePage(){
+  window.location.hash = '#/'
+}
+
+const passwordField = ref(null)
+
+function switchVisibility(){
+  console.log(passwordField.value.getAttribute('type'))
+  if (passwordField.value.getAttribute('type') === 'password'){
+    passwordField.value.setAttribute('type', 'text')
+  }
+  else {
+    passwordField.value.setAttribute('type', 'password')
+  }
+}
+
+</script>
+
+<template>
+  <p>Username: {{ message }}</p>
+  <input v-model="message" placeholder="Your username here" />
+  <p>Password:</p>
+  <input type="password" ref="passwordField" placeholder="Password">
+  <button @click="switchVisibility">show / hide</button>
+  <button @click="toHomePage">Log in</button>
+</template> 
