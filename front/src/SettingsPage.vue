825bd3d28eb5a1e25e630c0e3cd8deaf388e6e3d
RoryBenes <83403384+RoryBenes@users.noreply.github.com>
2023-04-04 21:35:53 +0200

    front: SettingsPage

--- front/src/SettingsPage.vue
+++ front/src/SettingsPage.vue
@@ -1,6 +1,12 @@
 <script setup>
-import { MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
+import { ref } from 'vue'
+import { MagnifyingGlassIcon, ChevronLeftIcon } from '@heroicons/vue/20/solid'
 import { QuestionMarkCircleIcon, AdjustmentsHorizontalIcon, ArrowRightCircleIcon } from '@heroicons/vue/24/outline'
+
+const userSettings = ref(false)
+const profileSettings = ref(false)
+const notificationsSettings = ref(false)
+
 
 </script>
 
@@ -13,25 +19,46 @@ import { QuestionMarkCircleIcon, AdjustmentsHorizontalIcon, ArrowRightCircleIcon
           <MagnifyingGlassIcon class="h-7 ml-4 mr-4"/>
           <input v-model="search" placeholder="Search Settings" class="h-12 bg-transparent -ml-8 focus:outline-none">
         </div>
+
         <div class="flex flex-col mt-8 items-center">
-            <button class="sidebarButton flex block w-64 items-center m-2">
-                <QuestionMarkCircleIcon class="h-6 mr-2"/>
-                <div>
-                    Nějakej setting
-                </div> 
-            </button>
-            <button class="sidebarButton flex block w-64 items-center m-2">
-                <AdjustmentsHorizontalIcon class="h-6 mr-2"/>
-                <div>
-                    Nějakej jinej setting
-                </div> 
-            </button>
-            <button class="sidebarButton flex block w-64 items-center m-2">
-                <ArrowRightCircleIcon class="h-6 mr-2"/>
-                <div>
-                    Bude jich tu spoustu
-                </div> 
-            </button>
+            <div v-if="userSettings" class="flex">
+                <h1>list of user settings</h1>
+                <button @click="userSettings = !userSettings" class="flex rounded-full w-15 h-15 margin-center items-center">
+                    <ChevronLeftIcon class="h-5 m-0"/>
+                </button>
+            </div>
+            <div v-else-if="profileSettings" class="flex">
+                <h1>list of profile settings</h1>
+                <button @click="profileSettings = !profileSettings" class="flex rounded-full w-15 h-15 margin-center items-center">
+                    <ChevronLeftIcon class="h-5 m-0"/>
+                </button>
+            </div>
+            <div v-else-if="notificationsSettings" class="flex">
+                <h1>list of notifications settings</h1>
+                <button @click="notificationsSettings = !notificationsSettings" class="flex rounded-full w-15 h-15 margin-center items-center">
+                    <ChevronLeftIcon class="h-5 m-0"/>
+                </button>
+            </div>
+            <div v-else>
+                <button @click="userSettings = !userSettings" class="sidebarButton flex block w-64 items-center m-2">
+                    <QuestionMarkCircleIcon class="h-6 mr-2"/>
+                    <div>
+                        User Settings
+                    </div> 
+                </button>
+                <button @click="profileSettings = !profileSettings" class="sidebarButton flex block w-64 items-center m-2">
+                    <AdjustmentsHorizontalIcon class="h-6 mr-2"/>
+                    <div>
+                        Profile Settings
+                    </div> 
+                </button>
+                <button @click="notificationsSettings = !notificationsSettings" class="sidebarButton flex block w-64 items-center m-2">
+                    <ArrowRightCircleIcon class="h-6 mr-2"/>
+                    <div>
+                        Notifications Settings
+                    </div> 
+                </button>
+            </div>
         </div>
     </div>
 
